cbc76c32e2e3ee00d9b80d01d7e4c20352609189
Richard's Mac <richardreal.rr@gmail.com>
2022-03-23 23:49:49 +0800

    updated

--- src/android/FakeLocation.java
+++ src/android/FakeLocation.java
@@ -1,23 +1,40 @@
-package tomloprod;
+package com.richardreal.fakelocation;
 
-import org.apache.cordova.CallbackContext;
-import org.apache.cordova.CordovaPlugin;
+// import org.apache.cordova.CallbackContext;
+// import org.apache.cordova.CordovaPlugin;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+import android.content.Context;
+import android.location.Location;
+import android.os.Build;
+import android.provider.Settings;
+
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.provider.Settings.Secure;
 
-public class FakeLocation extends CordovaPlugin {
-
-    @Override
-    public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
+@CapacitorPlugin(name = "FakeLocation")
+public class FakeLocation extends Plugin {
+    @PluginMethod
+    public boolean execute(String action, JSONArray data, Context context, Location location, PluginCall call) throws JSONException {
+        boolean isMock = false;
+        JSObject result = new JSObject();
         if (action.equals("check")) {
-			if (Secure.getString(this.cordova.getActivity().getContentResolver(), Secure.ALLOW_MOCK_LOCATION).equals("0")) {
-				callbackContext.success(0);
-			} else {
-				callbackContext.success(1);
-			}
+            if (Build.VERSION.SDK_INT >= 18) {
+                isMock = location.isFromMockProvider();
+            } else {
+                isMock = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ALLOW_MOCK_LOCATION).equals(0);
+            }
+
+            result.put("is_mock", isMock);
+
+            call.resolve(result);
             return true;
         }
-		return false;
+        return false;
     }
 }
